1d173a2901ffacd5b1a1295eba6a64663f09adf3
Amarsanthosh Santhosh <amarsanthoshsanthosh@gmail.com>
2025-12-02 10:23:16 +0530

    Added CSS

---
+++ App.js
@@ -0,0 +1,6 @@
+const heading2 = React.createElement("h1",{id:"heading"},"Hello WOrld from React..."); 
+const root2 = ReactDOM.createRoot(document.getElementById("root")); 
+
+console.log(heading2); //Object 
+
+root2.render(heading2);
