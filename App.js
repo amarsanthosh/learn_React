5c05722449d4a1029fb3e2a760f394830e474870
Amarsanthosh Santhosh <amarsanthoshsanthosh@gmail.com>
2025-12-02 10:34:25 +0530

    Nested Structure

--- App.js
+++ App.js
@@ -1,6 +1,29 @@
+
+/*
+<div id = "parent">
+    <div id = "child">
+        <h1> Iam an h1 tag </h1>
+        <h2> Iam an h2 tag </h2>
+    </div>
+</div>        
+
+*/
+
+
+
 const heading2 = React.createElement("h1",{id:"heading"},"Hello WOrld from React..."); 
 const root2 = ReactDOM.createRoot(document.getElementById("root")); 
+
+const nestedstruct = React.createElement("div",{id : "parent"}, React.createElement("div",{id:"child"},[
+    React.createElement("h1",{},"Iam h1 tag"), 
+    React.createElement("h2",{},"Iam h2 tag"),    
+])); 
+
+
 
 console.log(heading2); //Object 
 
 root2.render(heading2);
+root2.render(nestedstruct);
+root2.render(heading2);
+root2.render(nestedstruct);
