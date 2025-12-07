021edf2a14ea76575d9f0b33c0e70291ed0bfd15
Amarsanthosh Santhosh <amarsanthoshsanthosh@gmail.com>
2025-12-08 03:43:08 +0530

    JSX 2.0

--- App.js
+++ App.js
@@ -1,9 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; 
 
-const heading = <h1 className="root"> This is heading...</h1>
 
-const heading2 = React.createElement("h1",{},"This is heading2..."); 
+//React element
+const heading = <div><h1
+ className="root"> 
+This is heading...
+this lala
+</h1></div>
+
+
+//React Functional components are  JS function which return JSX or react element ! 
+
+// const Heading = () => {
+//     return <h2> This is a react Component </h2>
+// }
+
+const Title = () => (
+    <h1> The TiTle... </h1>
+);
+
+
+const Heading = () => (
+  <>
+    <h2>{heading}</h2>
+    <Title/>
+    <Title></Title>
+    {Title()} 
+
+    <h2>This is a react Component</h2>
+  </>
+);
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root")); 
 
@@ -11,6 +41,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
 console.log(heading);
-console.log(heading2);
 
 root.render(heading); 
+root.render(<Heading/>)
