710d7e4a1de25b06c31a8b9c2676df55d1573564
Amarsanthosh Santhosh <amarsanthoshsanthosh@gmail.com>
2025-12-08 20:48:51 +0530

    Basic res-container done

--- App.js
+++ App.js
@@ -1,51 +1,87 @@
-import React from "react";
-import ReactDOM from "react-dom/client"; 
+  import React from "react";
+  import ReactDOM from "react-dom/client"; 
 
-/**
- * Header 
- *  - logo
- *  - nav items
- * Body
- *  - search
- *  - restaurant container
- *      - restsurant card 
- * Footer 
- *  - copyright
- *  - Links
- *  - Address
- *  - contact
- */
+  /**
+   * Header 
+   *  - logo
+   *  - nav items
+   * Body
+   *  - search
+   *  - restaurant container
+   *      - restsurant card 
+   * Footer 
+   *  - copyright
+   *  - Links
+   *  - Address
+   *  - contact
+   */
 
-const Header = () => {
-  return (
-    <div className="header">
-      <div className="logo-container">
-        <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"/>
+  const Header = () => {
+    return (
+      <div className="header">
+        <div className="logo-container">
+          <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"/>
+        </div>
+        <div  className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
       </div>
-      <div  className="nav-items">
-      <ul>
-        <li>Home</li>
-        <li>About us</li>
-        <li>Contact us</li>
-        <li>Cart</li>
-      </ul>
-    </div>
-    </div>
-  );
-};
+      </div>
+    );
+  };
+
+
+  const RestaurantCard = () =>{
+    return (
+      <div className="res-card" style={{backgroundColor : "#f0f0f0"}}>
+        <img alt="res-image" className="res-image" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/fvxeg8wezgowcit6tsyo"/>
+        <h3>Sai Brindhavan</h3>
+        <h4>South Indian,Chinese</h4>
+        <h4>4.6 stars</h4>
+        <h4>25 mins</h4>
+      </div>
+    );
+  };
+
+
+  const Body = () => {
+    return (
+      <div className="body">
+        <div className="search">Search</div>
+        <div className="res-container">
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+        </div>
+      </div>
+    );
+  };
+
+
+  const AppLayout = () => {
+    return (
+      <div className="app">
+        <Header/>
+        <Body/>
+      </div>
+    );
+  };
 
 
 
-const AppLayout = () => {
-  return (
-    <div className="app">
-      <Header/>
-    </div>
-  );
-};
+  const root = ReactDOM.createRoot(document.getElementById("root")); 
 
-
-
-const root = ReactDOM.createRoot(document.getElementById("root")); 
-
-root.render(<AppLayout/>); 
+  root.render(<AppLayout/>); 
