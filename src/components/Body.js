4c57b1980e734039719e2584f15de3b873db74ab
Amarsanthosh Santhosh <amarsanthoshsanthosh@gmail.com>
2025-12-09 21:27:52 +0530

    Filter without react hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,11 +2,20 @@ import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 
 const Body = () => {
+    let reslist = resList;
   return (
     <div className="body">
       <div className="search">Search</div>
+      <div className="filter">
+        <button className="filter-btn"
+        onClick={()=>{
+            reslist = reslist.filter((res)=> res.data.avgRating > 4);
+            console.log(reslist); 
+        }}
+        >Top Restaurants</button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {reslist.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
