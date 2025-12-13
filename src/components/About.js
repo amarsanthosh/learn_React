85a9e166e555a9554c8e1ad6d131d4e6ef334006
Amarsanthosh Santhosh <amarsanthoshsanthosh@gmail.com>
2025-12-13 19:13:11 +0530

    Class Based Component init

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,10 @@
+import UserClass from "./UserClass";
+
 const About = () => {
     return (
         <div>
             <h1>This is About page...</h1>
+            <UserClass/>
         </div>
     )
 }
---
+++ src/components/UserClass.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class UserClass extends React.Component {
+
+    render(){
+        return(
+            <div className="user-class">
+                <h2>This is AMR</h2>
+                <h3>Internet engineer</h3>
+                <h4>Location : KMU</h4>
+            </div>
+        )
+    }
+}
+
+export default UserClass;  
