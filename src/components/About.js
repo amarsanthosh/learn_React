979016c0f48eef1f448465864c787b3e2f293d7e
Amarsanthosh Santhosh <amarsanthoshsanthosh@gmail.com>
2025-12-13 19:28:12 +0530

    used props in CBC

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div>
             <h1>This is About page...</h1>
-            <UserClass/>
+            <UserClass name={"AMR006"} location={"KMU"}/>
         </div>
     )
 }
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -2,12 +2,17 @@ import React from "react";
 
 class UserClass extends React.Component {
 
+    constructor(props){
+        super(props); 
+    }
+
     render(){
+        const {name, location} = this.props;
         return(
             <div className="user-class">
-                <h2>This is AMR</h2>
+                <h2>This is {name}</h2>
                 <h3>Internet engineer</h3>
-                <h4>Location : KMU</h4>
+                <h4>Location : {location}</h4>
             </div>
         )
     }
