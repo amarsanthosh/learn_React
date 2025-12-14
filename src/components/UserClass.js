3e5dd03463b5c8df3c8ead35026858a7c0f11c1f
Amarsanthosh Santhosh <amarsanthoshsanthosh@gmail.com>
2025-12-14 11:23:39 +0530

    CBC mount,update,unmount

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,15 +1,39 @@
 import React from "react";
 
 class UserClass extends React.Component {
-
+    
     constructor(props){
-        super(props); 
+        super(props);
+        this.state = {
+            userInfo : {
+                name : "dummy",
+                location : "default" 
+            }    
+        } 
+    }
+
+    async componentDidMount() {
+        const data = await fetch("https://api.github.com/users/akshaymarch7")
+        const json = await data.json();
+        this.setState({
+            userInfo : json
+        })
+    }
+
+    componentDidUpdate(){
+        console.log("Component updated !!!!"); 
+    }
+
+    componentWillUnmount(){
+        console.log("Component unmounted !!!")
     }
 
     render(){
-        const {name, location} = this.props;
+        // const {name, location} = this.props;
+        const {name, location } = this.state.userInfo; 
         return(
             <div className="user-class">
+             
                 <h2>This is {name}</h2>
                 <h3>Internet engineer</h3>
                 <h4>Location : {location}</h4>
